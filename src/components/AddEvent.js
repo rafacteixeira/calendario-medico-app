580c4a087f9a3ee81adbd42117b267efa459171e
Rafael Cruz <rafael.cruz@hotmart.com>
2022-10-24 13:39:04 -0300

    adds Pos Plantão Type

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -2,13 +2,21 @@ import {useContext, useState} from "react";
 import {SCContext} from "./SCContext";
 import moment from "moment";
 import DropdownList from "react-widgets/DropdownList";
-
-import "react-widgets/styles.css";
+import "../AddEvent.css";
 
 const AddEvent = ({selectedDate}) => {
   const [scContext, setScContext] = useContext(SCContext)
   const [type, setType] = useState("")
   const [watch, setWatch] = useState("")
+
+  function clearMorningEvents() {
+    return scContext.filter((current) => {
+        let mCurrDate = moment(current.Date)
+        let mSelectedDate = moment(selectedDate)
+        return !mCurrDate.isSame(mSelectedDate) || current.Watch !== 'manha'
+      }
+    )
+  }
 
   const addEvent = () => {
     let event = {
@@ -16,7 +24,14 @@ const AddEvent = ({selectedDate}) => {
       Type: type,
       Watch: watch
     }
-    localStorage.setItem("events", JSON.stringify([...scContext, event]))
+    let events
+    if (event.Type === 'PosP') {
+      events = clearMorningEvents()
+    } else {
+      events = [...scContext]
+    }
+
+    localStorage.setItem("events", JSON.stringify([...events, event]))
     setScContext(JSON.parse(localStorage.getItem('events')))
   }
 
@@ -37,6 +52,7 @@ const AddEvent = ({selectedDate}) => {
     {id: 'Enf', name: 'Enfermaria'},
     {id: 'Amb', name: 'Ambulatório'},
     {id: 'Pla', name: 'Plantão'},
+    {id: 'PosP', name: 'Pós Plantão'},
   ];
 
   let watches = [
